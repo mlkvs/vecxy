2ccb1753c66748d49e118177c89f8957bb16158a
Dmitry Melkov <melkov@itibsoft.ru>
2025-09-25 22:38:00 +0300

    - feat: first try draw triangle

--- Vecxy.Rendering/RenderPipeline.cs
+++ Vecxy.Rendering/RenderPipeline.cs
@@ -8,7 +8,17 @@ public class RenderPipeline
 {
     private WindowHandle? _window;
     private OpenGLContextHandle? _context;
+    private int _vao, _vbo;
+    private Shader _shader;
+
     public void Start()
+    {
+        InitializeWindow();
+        InitializeGraphics();
+        MainLoop();
+    }
+
+    private void InitializeWindow()
     {
         var options = new ToolkitOptions()
         {
@@ -18,9 +28,7 @@ public class RenderPipeline
         Toolkit.Init(options);
         
         var hints = new OpenGLGraphicsApiHints();
-
         _window = Toolkit.Window.Create(hints);
-
         _context = Toolkit.OpenGL.CreateFromWindow(_window);
         Toolkit.OpenGL.SetCurrentContext(_context);
         
@@ -28,24 +36,59 @@ public class RenderPipeline
         OpenTK.Graphics.GLLoader.LoadBindings(binding);
 
         var monitor = Monitors.GetPrimaryMonitor();
-
         var screenSize = monitor.ClientArea.Size;
         
         Toolkit.Window.SetSize(_window, 500, 500);
-        Toolkit.Window.SetPosition(_window, screenSize.X / 2 - 500 / 2 ,screenSize.Y / 2 - 500 / 2);
+        Toolkit.Window.SetPosition(_window, screenSize.X / 2 - 500 / 2, screenSize.Y / 2 - 500 / 2);
         Toolkit.Window.SetMode(_window, WindowMode.Normal);
 
         EventQueue.EventRaised += HandleEvents;
-        
-        
+    }
 
+    private void InitializeGraphics()
+    {
+        GL.ClearColor(0.2f, 0.3f, 0.4f, 1.0f);
+
+        // Создаем и компилируем шейдеры
+        _shader = new Shader();
+        _shader.Setup();
+
+        // Вершины треугольника
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f,  // левый нижний
+             0.5f, -0.5f, 0.0f,  // правый нижний
+             0.0f,  0.5f, 0.0f   // верхний центр
+        };
+
+        // Создаем VAO и VBO
+        _vao = GL.GenVertexArray();
+        _vbo = GL.GenBuffer();
+
+        GL.BindVertexArray(_vao);
+        
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+        GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), 
+                     vertices, BufferUsageHint.StaticDraw);
+
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+
+        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        GL.BindVertexArray(0);
+    }
+
+    private void MainLoop()
+    {
         while (true)
         {
-            GL.ClearColor(0.2f, 0.3f, 0.4f, 1.0f);
             GL.Clear(ClearBufferMask.ColorBufferBit);
             
-            Toolkit.OpenGL.SwapBuffers(_context);
+            // Используем шейдер и рисуем треугольник
+            GL.UseProgram(_shader.ProgramHandle);
+            GL.BindVertexArray(_vao);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
             
+            Toolkit.OpenGL.SwapBuffers(_context);
             Toolkit.Window.ProcessEvents(false);
 
             if (Toolkit.Window.IsWindowDestroyed(_window))
@@ -53,6 +96,15 @@ public class RenderPipeline
                 break;
             }
         }
+        
+        Cleanup();
+    }
+
+    private void Cleanup()
+    {
+        GL.DeleteVertexArray(_vao);
+        GL.DeleteBuffer(_vbo);
+        GL.DeleteProgram(_shader.ProgramHandle);
     }
 
     private void HandleEvents(PalHandle? handle, PlatformEventType type, EventArgs args)
---
+++ Vecxy.Rendering/Shader.cs
@@ -0,0 +1,76 @@
+﻿using OpenTK.Graphics.OpenGL;
+
+namespace Vecxy.Rendering;
+
+public class Shader
+{
+    private string _vertex = @"
+#version 330 core
+
+layout(location = 0) in vec3 aPosition;
+
+void main()
+{
+    gl_Position = vec4(aPosition, 1.0);
+}";
+
+    private string _fragment = @"
+#version 330 core
+
+out vec4 fragColor;
+
+void main()
+{
+    fragColor = vec4(1.0, 1.0, 1.0, 1.0); // Белый цвет
+}";
+
+    public int ProgramHandle { get; private set; }
+
+    public void Setup()
+    {
+        // Вершинный шейдер
+        var vertexHandle = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(vertexHandle, _vertex);
+        GL.CompileShader(vertexHandle);
+        CheckCompileErrors(vertexHandle, "VERTEX");
+
+        // Фрагментный шейдер
+        var fragmentHandle = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(fragmentHandle, _fragment);
+        GL.CompileShader(fragmentHandle);
+        CheckCompileErrors(fragmentHandle, "FRAGMENT");
+
+        // Шейдерная программа
+        ProgramHandle = GL.CreateProgram();
+        GL.AttachShader(ProgramHandle, vertexHandle);
+        GL.AttachShader(ProgramHandle, fragmentHandle);
+        GL.LinkProgram(ProgramHandle);
+        CheckLinkErrors(ProgramHandle);
+
+        // Очистка
+        GL.DetachShader(ProgramHandle, vertexHandle);
+        GL.DetachShader(ProgramHandle, fragmentHandle);
+        GL.DeleteShader(vertexHandle);
+        GL.DeleteShader(fragmentHandle);
+    }
+
+    private void CheckCompileErrors(int shader, string type)
+    {
+        GL.GetShader(shader, ShaderParameter.CompileStatus, out int success);
+        if (success == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(shader);
+            Console.WriteLine($"ERROR::SHADER_COMPILATION_ERROR of type: {type}\n{infoLog}\n");
+        }
+    }
+
+    private void CheckLinkErrors(int program)
+    {
+        GL.GetProgram(program, GetProgramParameterName.LinkStatus, out int success);
+        if (success == 0)
+        {
+            string infoLog = GL.GetProgramInfoLog(program);
+            Console.WriteLine($"ERROR::PROGRAM_LINKING_ERROR\n{infoLog}\n");
+        }
+    }
+}
