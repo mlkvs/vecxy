6bc6b27bc208fbd946f8aa1d3970870dba9681ba
Dmitry Melkov <melkov@itibsoft.ru>
2025-09-27 21:15:37 +0300

    - feat: create lib for reflection

---
+++ Vecxy.Reflection/EmbeddedResource/EmbeddedResourceUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+namespace Vecxy.Reflection;
+
+public static class EmbeddedResourceUtils
+{
+    public static EmbeddedResource? GetEmbeddedResource(this Assembly? assembly, string resourceName)
+    {
+        return EmbeddedResource.Get(assembly, resourceName);
+    }
+    
+    public static bool HasEmbeddedResource(this Assembly? assembly, string resourceName)
+    {
+        return EmbeddedResource.Has(assembly, resourceName);
+    }
+    
+    
+}
