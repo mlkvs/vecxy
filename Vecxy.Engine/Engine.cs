b5edc7675e2d8e84a5b9f9c766e0039ba3a462a8
Dmitry Melkov <melkov@itibsoft.ru>
2025-09-27 21:15:27 +0300

    - feat: create base engine lib and start app

---
+++ Vecxy.Engine/Engine.cs
@@ -0,0 +1,54 @@
+﻿using Vecxy.Rendering;
+
+namespace Vecxy.Engine;
+
+public class Engine : IDisposable
+{
+    private readonly RenderingWindow _renderingWindow;
+
+    public Engine()
+    {
+        var options = new RenderingWindowOptions
+        {
+            Width = 800,
+            Height = 600,
+            Title = "Vecxy.Rendering",
+        };
+        
+        _renderingWindow =  new RenderingWindow(options);
+        
+        _renderingWindow.Load += OnLoad;
+        _renderingWindow.UpdateFrame += OnUpdate;
+        _renderingWindow.RenderFrame += OnRenderingWindowRenderFrame;
+        _renderingWindow.Unload += OnUnload;
+    }
+
+    public void Run()
+    {
+        _renderingWindow.Run();
+    }
+
+    private void OnLoad()
+    {
+    }
+
+    private void OnUpdate(OpenTK.Windowing.Common.FrameEventArgs e)
+    {
+        var deltaTime = (float)e.Time;
+    }
+
+    private void OnRenderingWindowRenderFrame(OpenTK.Windowing.Common.FrameEventArgs e)
+    {
+        
+    }
+
+    private void OnUnload()
+    {
+
+    }
+
+    public void Dispose()
+    {
+        _renderingWindow.Dispose();
+    }
+}
