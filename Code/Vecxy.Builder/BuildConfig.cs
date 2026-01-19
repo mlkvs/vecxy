3988d30daaad03617be61301586094c793a7cdb3
Dmitry Melkov <melkov.dev@yandex.ru>
2026-01-19 23:14:04 +0700

    [Builder] Update logic for builder

---
+++ Code/Vecxy.Builder/BuildConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Vecxy.Builder;
+
+public enum BUILD_MODE : byte
+{
+    RELEASE,
+    DEBUG
+}
+
+[DataContract]
+public class BuildConfig
+{
+    [DataMember] public required string ProjectDir { get; set; }
+    [DataMember] public required string OutputDir { get; set; }
+    [DataMember] public required BUILD_MODE Mode { get; set; }
+}
