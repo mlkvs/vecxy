b3349540ac36796e30008641bbc5b752a9dfe046
Dmitry Melkov <melkov.dev@yandex.ru>
2026-09-11 17:36:46 +0700

    Build chunk and region spatial containers

---
+++ Code/Vecxy.Isometric/Core/Chunk.cs
@@ -0,0 +1,123 @@
+﻿using System.Numerics;
+
+namespace Vecxy.Isometric;
+
+public sealed class Chunk
+{
+    private readonly TileColumn[,] _columns;
+    private readonly WorldGridConfig _config;
+    private readonly HashSet<Guid> _objectIds = [];
+    private readonly HashSet<Guid> _wallIds = [];
+
+    internal Chunk(ChunkCoord coord, WorldGridConfig config)
+    {
+        Coord = coord;
+        Region = config.GetRegion(coord);
+        _config = config;
+        _columns = new TileColumn[config.ChunkWidth, config.ChunkHeight];
+
+        for (var y = 0; y < config.ChunkHeight; y++)
+        for (var x = 0; x < config.ChunkWidth; x++)
+        {
+            var tile = config.GetTile(coord, new LocalTileCoord(x, y));
+            _columns[x, y] = new TileColumn(tile.X, tile.Y, MarkChanged);
+        }
+
+        IsDirty = true;
+    }
+
+    public ChunkCoord Coord { get; }
+    public RegionCoord Region { get; }
+    public int Width => _columns.GetLength(0);
+    public int Height => _columns.GetLength(1);
+    public bool IsDirty { get; private set; }
+    public long ChangeVersion { get; private set; }
+    public IReadOnlySet<Guid> ObjectIds => _objectIds;
+    public IReadOnlySet<Guid> WallIds => _wallIds;
+
+    public TileColumn GetColumn(LocalTileCoord coord)
+    {
+        if (!_config.Contains(coord))
+            throw new ArgumentOutOfRangeException(nameof(coord));
+
+        return _columns[coord.X, coord.Y];
+    }
+
+    public bool TryGetTile(TileCoord coord, out Tile? tile)
+    {
+        if (_config.GetChunk(coord) != Coord)
+        {
+            tile = null;
+            return false;
+        }
+
+        return GetColumn(_config.GetLocalTile(coord)).TryGetLevel(coord.Level, out tile);
+    }
+
+    internal Tile GetOrCreateTile(TileCoord coord)
+    {
+        if (_config.GetChunk(coord) != Coord)
+            throw new ArgumentOutOfRangeException(nameof(coord), "Tile belongs to another chunk.");
+
+        return GetColumn(_config.GetLocalTile(coord)).GetOrCreateLevel(coord.Level);
+    }
+
+    internal void RegisterObject(Guid objectId)
+    {
+        _objectIds.Add(objectId);
+    }
+
+    internal void UnregisterObject(Guid objectId)
+    {
+        _objectIds.Remove(objectId);
+    }
+
+    internal void RegisterWall(Guid wallId) => _wallIds.Add(wallId);
+
+    internal void UnregisterWall(Guid wallId) => _wallIds.Remove(wallId);
+
+    internal ChunkData CaptureData()
+    {
+        var tiles = new List<TileData>();
+        for (var y = 0; y < Height; y++)
+        for (var x = 0; x < Width; x++)
+        {
+            foreach (var tile in _columns[x, y].Levels.Where(tile => tile.Surface is not null))
+                tiles.Add(new TileData(tile.Coord, tile.Surface?.Type));
+        }
+
+        return new ChunkData(Coord, [.. tiles]);
+    }
+
+    internal void ApplyData(ChunkData data)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+        if (data.Coord != Coord)
+            throw new ArgumentException("Chunk data belongs to another chunk.", nameof(data));
+
+        foreach (var source in data.Tiles)
+        {
+            if (_config.GetChunk(source.Coord) != Coord)
+                throw new ArgumentException("Chunk data contains a tile from another chunk.", nameof(data));
+
+            var tile = GetOrCreateTile(source.Coord);
+            tile.SetSurface(source.Surface is { } type ? new TileSurface(type) : null);
+        }
+
+        IsDirty = false;
+    }
+
+    internal void MarkPersisted(long version)
+    {
+        if (ChangeVersion == version)
+            IsDirty = false;
+    }
+
+    internal void MarkLoaded() => IsDirty = false;
+
+    private void MarkChanged()
+    {
+        ChangeVersion++;
+        IsDirty = true;
+    }
+}
---
+++ Code/Vecxy.Isometric/Core/Region.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+
+namespace Vecxy.Isometric;
+
+public sealed class Region
+{
+    private readonly Dictionary<ChunkCoord, Chunk> _chunks = [];
+    private readonly WorldGridConfig _config;
+
+    internal Region(RegionCoord coord, WorldGridConfig config)
+    {
+        Coord = coord;
+        _config = config;
+    }
+
+    public RegionCoord Coord { get; }
+    public IReadOnlyCollection<Chunk> Chunks => _chunks.Values;
+
+    public bool TryGetChunk(ChunkCoord coord, out Chunk? chunk)
+    {
+        if (_config.GetRegion(coord) != Coord)
+        {
+            chunk = null;
+            return false;
+        }
+
+        return _chunks.TryGetValue(coord, out chunk);
+    }
+
+    internal Chunk GetOrCreateChunk(ChunkCoord coord)
+    {
+        if (_config.GetRegion(coord) != Coord)
+            throw new ArgumentOutOfRangeException(nameof(coord), "Chunk belongs to another region.");
+
+        if (_chunks.TryGetValue(coord, out var chunk))
+            return chunk;
+
+        chunk = new Chunk(coord, _config);
+        _chunks.Add(coord, chunk);
+        return chunk;
+    }
+
+    internal bool RemoveChunk(ChunkCoord coord) => _chunks.Remove(coord);
+}
